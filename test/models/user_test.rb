e598ae98c4003c65b0870c455b8ea65937e8f4ec
金子 翼 <gamuuma@gmail.com>
2016-11-20 22:28:29 +0900

    has_secure_passwordメソッドを使って暗号化

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 	def setup
-		@user = User.new(name:"Okashiki Kenji", email:"kenji.okashiki@rakus.co.jp")
+		@user = User.new(name:"Okashiki Kenji", email:"kenji.okashiki@rakus.co.jp", password:"foober", password_confirmation:"foober")
 	end
 
 	test  "should be valid?" do
@@ -44,5 +44,15 @@ class UserTest < ActiveSupport::TestCase
 		duplicate_user.email = @user.email.upcase
 		@user.save
 	  assert_not duplicate_user.valid?
+	end
+
+	test "password should be present (nonblank)" do
+	  @user.password = @user.password_confirmation = "" * 6
+		assert_not @user.valid?
+	end
+
+	test "password should have a minimum length" do
+	  @user.password = @user.password_confirmation = "a" * 5
+		assert_not @user.valid?
 	end
 end
