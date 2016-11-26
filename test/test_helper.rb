013c8203764eb18c1ffeac1d72b0007a1e4b2152
金子 翼 <gamuuma@gmail.com>
2016-11-27 00:15:54 +0900

    ログインまで

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,5 +9,10 @@ class ActiveSupport::TestCase
   # order.
   fixtures :all
 
+  # テストユーザーがログインしていればtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
   # Add more helper methods to be used by all tests here...
 end
