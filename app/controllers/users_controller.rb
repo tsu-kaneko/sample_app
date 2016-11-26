8e672f55021e46b321583ed5d0938f81249b1a30
金子 翼 <gamuuma@gmail.com>
2016-11-26 13:35:12 +0900

    無効なユーザー登録に対するテスト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # 実装は終わっていないことに注意!
     if @user.save
-      # Handle a successful save.
+      redirect_to @user
     else
       render 'new'
     end
