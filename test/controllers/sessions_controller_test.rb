ea0d66378e090675d66c5ea1106bbb1919731317
tsu-kaneko <tsu.kaneko0410@gmail.com>
2017-01-02 20:41:00 +0900

    8章終わりまで

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
   test "should get new" do
-    get :new
+    get login_path
     assert_response :success
   end
 
