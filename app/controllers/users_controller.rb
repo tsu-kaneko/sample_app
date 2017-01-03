fbf933b63c74c6688df9dc4cd1d75bdb88caec42
tsu-kaneko <tsu.kaneko0410@gmail.com>
2017-01-03 22:35:29 +0900

    9章終わり削除機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only:[:index, :edit, :update]
+  before_action :logged_in_user, only:[:index, :edit, :update, :destroy]
   before_action :correct_user, only:[:edit, :update]
 
   def index
     @users = User.paginate(page: params[:page])
   end
-  
+
   def new
     @user = User.new
   end
@@ -38,6 +38,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
 
   private
     # Strong Parameterを使用している
@@ -59,5 +65,10 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # 管理者かどうかの確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
